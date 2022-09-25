f681b1f29746efb28e888c59a94404e931054658
luckypaquette <11478691+luckypaquette@users.noreply.github.com>
2022-09-25 15:18:55 -0400

    Corrections au script pour valider avant de traiter les données

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 /* Fonction calcul */
 function fCalcul() {
     // déclaration Variables
-    var NomEtudiant = document.RegForm.nometudiant.value;
-    var NumMatricule = parseInt(document.RegForm.nummatricule.value)
-    var NoteExam1 = parseFloat(document.RegForm.noteexam1.value)
-    var NoteExam2 = parseFloat(document.RegForm.noteexam2.value)
-    var NoteExam3 = parseFloat(document.RegForm.noteexam3.value)
-    var NoteTravail1 = parseFloat(document.RegForm.notetravail1.value)
-    var NoteTravail2 = parseFloat(document.RegForm.notetravail2.value)
-    var NoteTravail3 = parseFloat(document.RegForm.notetravail3.value)
-    var NoteTravail4 = parseFloat(document.RegForm.notetravail4.value)
+    var NomEtudiant;
+    var NumMatricule;
+    var NoteExam1;
+    var NoteExam2;
+    var NoteExam3;
+    var NoteTravail1;
+    var NoteTravail2;
+    var NoteTravail3;
+    var NoteTravail4;
     var EnsMoyExam;
     var EnsMoyTravaux;
     var Resultat;
@@ -22,27 +22,112 @@ function fCalcul() {
         document.RegForm.nometudiant.focus();
         return false;
     }
-    if (NumeroMatricule == '') {
-
+    if (NumMatricule == '') {
         alert('Veuillez insérer un nombre composé de sept chiffres.');
         document.RegForm.nummatricule.value = ''
-        document.RegForm.nummatricule.focus()
+        document.RegForm.nummatricule.focus();
         return false;
     }
+    // validation
+    if (NomEtudiant == "") {
+        alert("Inscrivez le nom de l'étudiant; Le champ est présentement Vide.");
+        document.RegForm.nometudiant.focus();
+        return false;
+    }
+    if (NumMatricule == '') {
+        alert('Veuillez insérer un nombre constitué de sept chiffres.');
+        document.RegForm.nummatricule.value = '';
+        document.RegForm.nummatricule.focus();
+        return false;
+    }    
 
-    EnsMoyExam = (NoteExam1 + NoteExam2 + NoteExam3) * 80 / 70
-    EnsMoyTravaux = (NoteTravail1 + NoteTravail2 + NoteTravail3 + NoteTravail4) * 20 / 65
-    Resultat = EnsMoyTravaux + EnsMoyExam
-    document.getElementById("Resultat").value = Resultat
+//modifié pour valider avant de parser
+//   SI va dans le document, le formulaire, le champ noteexam1 va chercher sa valeur, 
+//   s'il contient une virgule INDEXOF va retourner la valeur 0 dans l'index de la 
+//   virgule dans la chaine de caractère.
+    if (document.RegForm.noteexam1.value.indexOf(',')>=0) {
+        alert('Veuillez entrer tout nombre décimal en le séparant de la portion entière avec le point (.) plutôt que la virgule(,)');
+        document.RegForm.noteexam1.value = ''
+        document.RegForm.noteexam1.focus();
+        return false;
+    }
+    if (document.RegForm.noteexam2.value.indexOf(',')>=0) {
+        alert('Veuillez entrer tout nombre décimal en le séparant de la portion entière avec le point (.) plutôt que la virgule(,)');
+        document.RegForm.noteexam2.value = '';
+        document.RegForm.noteexam2.focus();
+        return false;
+    }
+    if (document.RegForm.noteexam3.value.indexOf(',')>=0) {
+        alert('Veuillez entrer tout nombre décimal en le séparant de la portion entière avec le point (.) plutôt que la virgule(,)');
+        document.RegForm.noteexam3.value = '';
+        document.RegForm.noteexam3.focus();
+        return false;
+    }
+    //à modifier comme ci-haut
+    if (document.RegForm.notetravail1.value.indexOf(',')>=0) {
+        alert('Veuillez entrer tout nombre décimal en le séparant de la portion entière avec le point (.) plutôt que la virgule(,)');
+        document.RegForm.notetravail1.value = ''
+        document.RegForm.notetravail1.focus();
+        return false;
+    }
+       if (document.RegForm.notetravail2.value.indexOf(',')>=0) {
+        alert('Veuillez entrer tout nombre décimal en le séparant de la portion entière avec le point (.) plutôt que la virgule(,)');
+    document.RegForm.notetravail2.value = '';
+    document.RegForm.notetravail2.focus();
+    return false;
+}    
+if (document.RegForm.notetravail3.value.indexOf(',')>=0) {
+    alert('Veuillez entrer tout nombre décimal en le séparant de la portion entière avec le point (.) plutôt que la virgule(,)');
+    document.RegForm.notetravail3.value = '';
+    document.RegForm.notetravail3.focus();
+    return false;
+}    
+if ( document.RegForm.notetravail4.value.indexOf(',')>=0) {
+    alert('Veuillez entrer tout nombre décimal en le séparant de la portion entière avec le point (.) plutôt que la virgule(,)');
+    document.RegForm.notetravail4.value = '';
+    document.RegForm.notetravail4.focus();
+    return false;
+}
+
+ NomEtudiant = document.RegForm.nometudiant.value;
+ NumMatricule = parseInt(document.RegForm.nummatricule.value);
+
+ NoteExam1 = parseFloat(document.RegForm.noteexam1.value);
+ NoteExam2 = parseFloat(document.RegForm.noteexam2.value);
+ NoteExam3 = parseFloat(document.RegForm.noteexam3.value);
+ NoteTravail1 = parseFloat(document.RegForm.notetravail1.value);
+ NoteTravail2 = parseFloat(document.RegForm.notetravail2.value);
+ NoteTravail3 = parseFloat(document.RegForm.notetravail3.value);
+ NoteTravail4 = parseFloat(document.RegForm.notetravail4.value);
+
+
+    EnsMoyExam = (NoteExam1 + NoteExam2 + NoteExam3) * 80 / 70;
+    EnsMoyTravaux = (NoteTravail1 + NoteTravail2 + NoteTravail3 + NoteTravail4) * 20 / 65;
+    Resultat = EnsMoyTravaux + EnsMoyExam;
+    document.getElementById("Resultat").value = Resultat;
 // resultat de la valeur
     document.getElementById("Resultat").value = Resultat.toFixed(2);
    
     // conditionnelle Mention
     if (Resultat >= 60) {
-        document.getElementById("Mention").innerHTML = "Réussite"
+        document.getElementById("Mention").innerHTML = "Réussite";
     }
     else {
-        document.getElementById("Mention").innerHTML = "Échec"
+        document.getElementById("Mention").innerHTML = "Échec";
+    }
+
+    EnsMoyExam = (NoteExam1 + NoteExam2 + NoteExam3) * 80 / 70;
+    EnsMoyTravaux = (NoteTravail1 + NoteTravail2 + NoteTravail3 + NoteTravail4) * 20 / 65;
+    Resultat = EnsMoyTravaux + EnsMoyExam;
+// resultat de la valeur
+    document.getElementById("Resultat").value = Resultat.toFixed(2);
+   
+    // conditionnelle Mention
+    if (Resultat >= 60) {
+        document.getElementById("Mention").innerHTML = "Réussite";
+    }
+    else {
+        document.getElementById("Mention").innerHTML = "Échec";
     }
     
 }
